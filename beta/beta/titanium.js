e1b96d2d5ef7ec2b41b851ae17ba28834620eb4b
rappatic <32337887+rappatic@users.noreply.github.com>
2018-09-02 13:06:25 +0100

    Updated navbar
    Updated to the most recent version of the navbar (includes js, eek)

---
+++ beta/beta/titanium.js
@@ -0,0 +1,9 @@
+// Titanium JS
+// Scary indeed
+
+// JS for responsive navbar
+
+function compressNavbar() {
+  var x = document.getElementById("navbar");
+  x.classList.toggle("responsive-h2-expand");
+}
