a7798441fbe272bc9a4456339dbfb4082fa06510
rappatic <32337887+rappatic@users.noreply.github.com>
2018-09-03 11:45:52 +0100

    Add files via upload

---
+++ titanium.js
@@ -0,0 +1,9 @@
+// Titanium JS
+// Scary indeed
+
+// JS for responsive horizontal navbar
+
+function compressNavbar() {
+  var x = document.getElementById("navbar-responsive");
+  x.classList.toggle("responsive-2-expand");
+}
